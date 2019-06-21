a09571fa6d5f58123d3263b4284902e43b703b18
esneidersm <esneider.marulanda@gmail.com>
2019-06-21 08:39:50 -0500

    Add login styles and call rick and morty API

--- src/pages/Expenses.js
+++ src/pages/Expenses.js
@@ -1,6 +1,22 @@
 import React from 'react'
+import axios from 'axios'
 
 class Expenses extends React.Component {
+    state = {
+        expenses: []
+    }
+
+    componentDidMount() {
+        axios.get('https://rickandmortyapi.com/api/character/')
+            .then(res => {
+
+                debugger
+                this.setState({
+                    expenses: res.data.results
+                })
+            }).catch(err => console.log(err))
+    }
+
     render() {
         return <h1>Gastos</h1>
     }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../styles/Login.css';
 
 class Login extends React.Component {
+
+    constructor(props, context) {
+        super(props, context);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        console.log("Click!!")
+    }
+
     render() {
-        return <h1>Login</h1>
+        return (
+            <div className="login">
+                <h1>Login</h1>
+                <form className="login__form">
+
+                    <div className="control-form">
+                        <input type="text" placeholder="Usuario" />
+                    </div>
+
+                    <div className="control-form">
+                        <input type="password" placeholder="Contraseña" />
+                    </div>
+
+                    <div className="control-form">
+                        <Link to="/home">
+                            <button className="button button-white">Ingresar</button>
+                        </Link>
+                    </div>
+
+                </form>
+            </div>
+        )
     }
 }
 export default Login;
